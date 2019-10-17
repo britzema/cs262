b7cce1a8dd18b24e0dc32446762d11e0dc534355
Brad Ritzema <bradritzema@gmail.com>
2019-10-17 16:24:15 -0400

    Added Homework02

---
+++ Homework02/app/src/main/java/edu/calvin/cs262/bdr22/SourceCodeLoader.java
@@ -0,0 +1,33 @@
+package edu.calvin.cs262.bdr22;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class SourceCodeLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+    private String mTransferProtocol;
+    Context mContext;
+
+    public SourceCodeLoader(@NonNull Context context, String queryString, String transferProtocol) {
+        super(context);
+        mContext = context;
+        mQueryString = queryString;
+        mTransferProtocol = transferProtocol;
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getSourceCode(mContext, mQueryString, mTransferProtocol);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+}
+
